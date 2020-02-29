192997e2134cd08f2e1a98581a2a29e13d02ea8d
Louis Jenkins <louisjenkinscs@pop-os.localdomain>
2020-02-29 14:43:56 -0500

    Making solid progress on the DurableQueue

--- pmat/tests/microbenchmarks/durable_queue/DurableQueue.cpp
+++ pmat/tests/microbenchmarks/durable_queue/DurableQueue.cpp
@@ -1,5 +1,8 @@
 #include "DurableQueue.hpp"
 
 int main(void) {
-    auto dq = DurableQueue::alloc<int>(new uint8_t[1024], 1024);
+    auto dq = DurableQueue::alloc<int, -1>(new uint8_t[1024], 1024);
+    #pragma omp parallel for
+    for (int i = 0; i < 10; i++) assert(dq->enqueue(1));
+    std::cout << dq << std::endl;
 }
--- pmat/tests/microbenchmarks/durable_queue/DurableQueue.hpp
+++ pmat/tests/microbenchmarks/durable_queue/DurableQueue.hpp
@@ -4,6 +4,8 @@
 #include <cstdbool>
 #include <vector>
 #include <iostream>
+#include <atomic>
+#include <cassert>
 
 #define DQ_MAX_THREADS 16
 #define DQ_EMPTY -1
@@ -17,6 +19,8 @@ namespace DurableQueue
             typedef struct uint128 {
                 uint64_t lo;
                 uint64_t hi;
+
+                uint128() : lo(0), hi(0) {}
 
                 uint64_t get_ptr(void) { return lo; }
                 uint64_t get_seq(void) { return hi; }
@@ -42,6 +46,11 @@ namespace DurableQueue
                     "m" (result)
                     : "cc", "memory"); 
                 return result;
+            }
+
+            static inline int cas128bit_special(uint128_t *src, uint128_t *cmp, uint128_t *with) {
+                with->hi = cmp->hi + 1;
+                return cas128bit(src, cmp, with);
             }
 
             static inline void write128bit(void *srcvp, void *valvp) {
@@ -120,9 +129,15 @@ namespace DurableQueue
                     }
                     T& operator*() { return *reinterpret_cast<T*>(ptr.get_ptr()); }
                     T* operator->() { return reinterpret_cast<T*>(ptr.get_ptr()); }
-                    operator T*() { return reinterpret_cast<T*>(ptr.get_ptr()); }
+                    // operator T*() { return reinterpret_cast<T*>(ptr.get_ptr()); }
+                    // operator T() { return *reinterpret_cast<T*>(ptr.get_ptr()); }
                     uint64_t get_seq() { return ptr.get_seq(); }
-                    uint128_t& to_ptr() { return ptr; }
+                    T *to_ptr() { return reinterpret_cast<T*>(ptr.get_ptr()); }
+                    uint128_t& to_raw_ptr() { return ptr; }
+
+                    bool operator==(ABA<T> other) {
+                        return this->ptr.lo == other.ptr.lo && this->ptr.hi == other.ptr.hi;
+                    }
                 private:
                     uint128_t __attribute__ ((aligned (16))) ptr;
             };
@@ -132,20 +147,36 @@ namespace DurableQueue
             class ABAPtr {
                 public:
                     bool CAS(ABA<T>& expected, ABA<T>& with) {
-                        uint128_t& abaRef = aba.to_ptr();
-                        uint128_t& expectedRef = expected.to_ptr();
-                        uint128_t& withRef = with.to_ptr();
+                        uint128_t& abaRef = aba.to_raw_ptr();
+                        uint128_t& expectedRef = expected.to_raw_ptr();
+                        uint128_t& withRef = with.to_raw_ptr();
                         return DWord::cas128bit(&abaRef, &expectedRef, &withRef);
                     }
 
+                    bool CAS(ABA<T>& expected, T *with) {
+                        uint128_t& abaRef = aba.to_raw_ptr();
+                        uint128_t& expectedRef = expected.to_raw_ptr();
+                        uint128_t withRef;
+                        withRef.set_ptr(reinterpret_cast<uint64_t>(with));
+                        return DWord::cas128bit_special(&abaRef, &expectedRef, &withRef);
+
+                    }
+
                     void store(ABA<T>& value) {
-                        uint128_t& abaRef = aba.to_ptr();
-                        uint128_t& valueRef = value.to_ptr();
+                        uint128_t& abaRef = aba.to_raw_ptr();
+                        uint128_t& valueRef = value.to_raw_ptr();
                         DWord::write128bit(&abaRef, &valueRef);
                     }
 
+                    void store(T *value) {
+                        uint128_t& abaRef = aba.to_raw_ptr();
+                        uint128_t valueRef;
+                        valueRef.set_ptr(reinterpret_cast<uint64_t>(value));
+                        DWord::write128bit_special(&abaRef, &valueRef);
+                    }
+
                     ABA<T> load() {
-                        uint128_t& abaRef = aba.to_ptr();
+                        uint128_t& abaRef = aba.to_raw_ptr();
                         ABA<T> aba;
                         DWord::read128bit(&abaRef, &aba);
                         return aba;
@@ -156,63 +187,108 @@ namespace DurableQueue
                     }
 
                     void operator=(T *other) {
-                        uint128_t& abaRef = aba.to_ptr();
+                        uint128_t& abaRef = aba.to_raw_ptr();
                         uint128_t valueRef;
                         valueRef.set_ptr(reinterpret_cast<uint64_t>(other));
                         DWord::write128bit_special(&abaRef, &valueRef);
                     }
+
+                    template <class R>
+                    friend std::ostream& operator<<(std::ostream& os, ABA<R> aba);
                 private:
                     ABA<T> aba;
             };
+            template <class T>
+            std::ostream& operator<<(std::ostream& os, ABA<T> aba) {
+                os << "(" << aba.get_seq() << ", " << aba.to_ptr() << ")";
+                return os;
+            }
         }
 
-        template <class T>
+        template <class T, T NilT = T()>
         class FreeListNode {
             public:
                 FreeListNode(T t) : obj(t), next(nullptr) {}
-                T& get_obj() { return obj; }
-                FreeListNode<T> *get_next() { return next; }
-                void set_next(FreeListNode<T> *node) { next = node; }
-            private:
+                FreeListNode() : obj(NilT), next(nullptr) {}
+                
+                template <class R>
+                friend std::ostream& operator<<(std::ostream& os, FreeListNode<R>*  freeList);
+            
                 T obj;
                 FreeListNode<T> *next;
         };
 
+        template <class T>
+        std::ostream& operator<<(std::ostream& os, FreeListNode<T>* freeList) {
+            if (freeList == nullptr) {
+                os << "[NULL]";
+            } else {
+                os << "(" << freeList->obj << ", " << reinterpret_cast<void *>(freeList->next) << ")";
+            }
+            return os;
+        }
+
         // FreeList is a trieber stack which used tagged/counted pointers.
         // Note: No new nodes will be created (for now...)
         // TODO: Build custom allocator for fun!
-        template <class T>
+        template <class T, T NilT = T()>
         class FreeList {
             public:
                 void push(T t) {
-                    Memory::DWord::ABA<FreeListNode<T>> node(new FreeListNode<T>(t));
+                    FreeListNode<T> *node = new FreeListNode<T>(t);
                     Memory::DWord::ABA<FreeListNode<T>> _head;
                     do {
                         _head = head.load();
-                        node->set_next(_head);
+                        node->next = _head.to_ptr();
                     } while(!head.CAS(_head, node));
+                    std::cerr << head.load() << std::endl;
+                }
+
+                T pop() {
+                    std::cout << head.load() << std::endl;
+                    Memory::DWord::ABA<FreeListNode<T>> _head;
+                    FreeListNode<T> *next;
+                    do {
+                        _head = head.load();
+                        if (_head.to_ptr() == nullptr) {
+                            std::cerr << _head << std::endl;
+                            return NilT;
+                        }
+                        next = _head->next;
+                    } while(!head.CAS(_head, next));
+                    return _head->obj; // Note this leaks!
                 }
 
             private:
                Memory::DWord::ABAPtr<FreeListNode<T>> head;
+
             
         };
     }
     
-    template <class T>
+    template <class T, T NilT = T()>
     class DurableQueueNode {
         public:
-            DurableQueueNode() : obj(), next(nullptr) {}
+            DurableQueueNode() : obj(NilT), next(nullptr), deqThreadID(-1) {}
             DurableQueueNode(T& t) : obj(t), next(nullptr) {}
 
-            T get_obj() { return obj; }
-            DurableQueueNode<T> *get_next() { return next; }
-            void set_obj(T& obj) { return obj; }
-            void set_next(DurableQueueNode<T> *node) { next = node; }
-        private:
+            template <class R, R NilR>
+            friend std::ostream& operator<<(std::ostream& os, DurableQueueNode<R, NilR> *node);
+
             T obj;
-            DurableQueueNode<T> *next;    
+            std::atomic<DurableQueueNode<T, NilT> *> next;
+            std::atomic<int> deqThreadID;
     };
+
+    template <class T, T NilT>
+    std::ostream& operator<<(std::ostream& os, DurableQueueNode<T, NilT> *node) {
+        if (node == nullptr) {
+            os << "[NULL]";
+        } else {
+            os << "(" << node->obj << ", " << reinterpret_cast<void *>(node->next.load()) << ", " << node->deqThreadID << ")";
+        }
+        return os;
+    }
 
     template <class T, T NilT = T()>
     class DurableQueue {
@@ -221,14 +297,14 @@ namespace DurableQueue
             // are utilizing to create nodes from.
             DurableQueue(uint8_t *heap, size_t sz) {
                 this->heap = heap;
-                this->freeList = new Memory::FreeList<DurableQueueNode<T> *>();
-                this->allocList = new std::vector<DurableQueueNode<T> *>();
+                this->freeList = new Memory::FreeList<DurableQueueNode<T, NilT> *>();
+                this->allocList = new std::vector<DurableQueueNode<T, NilT> *>();
 
                 // Initialize allocList
                 int allocSz = 0;
-                while (allocSz + sizeof(DurableQueueNode<T>) < sz) {
-                    this->allocList->push_back(new (heap + allocSz) DurableQueueNode<T>());
-                    allocSz += sizeof(DurableQueueNode<T>);
+                while (allocSz + sizeof(DurableQueueNode<T, NilT>) < sz) {
+                    this->allocList->push_back(new (heap + allocSz) DurableQueueNode<T, NilT>());
+                    allocSz += sizeof(DurableQueueNode<T, NilT>);
                 }
                 
                 // Initialize freeList
@@ -243,28 +319,74 @@ namespace DurableQueue
 
                 // Initialize head and tail with dummy node
                 T tmp = NilT;
-                DurableQueueNode<T> *dummy = new DurableQueueNode<T>(tmp);
-                this->head = dummy;
-                this->tail = dummy;
+                DurableQueueNode<T, NilT> *dummy = freeList->pop();
+                dummy->obj = NilT;
+                this->head.store(dummy);
+                this->tail.store(dummy);
             }
+
+            bool enqueue(T t) {
+                DurableQueueNode<T, NilT> *node = freeList->pop();
+                if (!node) return false;
+                node->deqThreadID = -1;
+                node->next = nullptr;
+                node->obj = NilT;
+                
+                while (true) {
+                    Memory::DWord::ABA<DurableQueueNode<T, NilT>> last = tail.load();
+                    std::cerr << this->tail.load() << std::endl;
+                    std::cerr << this->head.load() << std::endl;
+                    assert(last.to_ptr() != nullptr);
+                    DurableQueueNode<T, NilT> *next = last->next;
+                    if (last == tail.load()) {
+                        if (next == nullptr) {
+                            if (last.to_ptr()->next.compare_exchange_strong(next, node)) {
+                                tail.CAS(last, node);
+                                return true;
+                            }
+                        } else {
+                            tail.CAS(last, next);
+                        }
+                    }
+                }
+
+                return true;
+            }
+            
+            template <class R, R NilR>
+            friend std::ostream& operator<<(std::ostream& os, DurableQueue<R, NilR> *dq);
 
         private: 
             // Persistent head of queue
-            Memory::DWord::ABAPtr<DurableQueueNode<T>> head;
+            Memory::DWord::ABAPtr<DurableQueueNode<T, NilT>> head;
             // Persistent tail of queue
-            Memory::DWord::ABAPtr<DurableQueueNode<T>> tail;
+            Memory::DWord::ABAPtr<DurableQueueNode<T, NilT>> tail;
             // Persistent array of return values for threads
             T retvals[DQ_MAX_THREADS];
             // Transient remaining portion of heap
             uint8_t *heap;
             // Transient FreeList
-            Memory::FreeList<DurableQueueNode<T>*> *freeList;
+            Memory::FreeList<DurableQueueNode<T, NilT>*> *freeList;
             // Transient AllocList
-            std::vector<DurableQueueNode<T>*>* allocList;                        
+            std::vector<DurableQueueNode<T, NilT>*>* allocList;                        
     };
 
-    template <class T>
-    static DurableQueue<T> *alloc(uint8_t *heap, size_t sz) {
-        return new (heap) DurableQueue<T>(heap + sizeof(DurableQueue<T>), sz - sizeof(DurableQueue<T>));
+    // Note: Not thread safe...
+    template <class T, T NilT>
+    std::ostream& operator<<(std::ostream& os, DurableQueue<T, NilT> *dq) {
+        DurableQueueNode<T, NilT> *head = dq->head.load().to_ptr();
+        DurableQueueNode<T, NilT> *tail = dq->tail.load().to_ptr();
+        os << "{ ";
+        while (head != tail) {
+            os << head->next << " ";
+            head = head->next.load();
+        }
+        os << "}";
+        return os;
+    }
+
+    template <class T, T NilT = T()>
+    static DurableQueue<T, NilT> *alloc(uint8_t *heap, size_t sz) {
+        return new (heap) DurableQueue<T, NilT>(heap + sizeof(DurableQueue<T, NilT>), sz - sizeof(DurableQueue<T, NilT>));
     }
 }
