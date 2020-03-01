d1f13a796c3d1a2ed7308c82a250eca98e763049
Louis Jenkins <louisjenkinscs@pop-os.localdomain>
2020-03-01 13:57:59 -0500

    Now have full benchmark, need to add persistence

--- pmat/tests/microbenchmarks/durable_queue/DurableQueue.cpp
+++ pmat/tests/microbenchmarks/durable_queue/DurableQueue.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <sstream>
 #include <cassert>
+#include <random>
 
 #define DQ_NULL -1
 #define DQ_EMPTY -2
@@ -33,20 +34,77 @@ void sanity_check(DurableQueue::DurableQueue<T, NilT, EmptyT> *dq, size_t numNod
     }
 }
 
+template <class T, T NilT, T EmptyT>
+void do_benchmark(DurableQueue::DurableQueue<T, NilT, EmptyT> *dq, long seconds) {
+    long numOperations = 0;
+    time_t start;
+    time_t end;
+	time(&start);
+    std::atomic<bool> stopSignal;
+    #pragma omp parallel reduction(+: numOperations)
+    {
+        long tid = omp_get_thread_num();
+        std::mt19937_64 rng;
+        rng.seed(tid);
+        #pragma omp master
+        std::cout << "Threads running: " << omp_get_num_threads() << std::endl;
+
+        while (!stopSignal.load()) {
+            #pragma omp master
+			{
+				time(&end);
+				long time_taken = end - start;
+
+				if (time_taken >= seconds) {
+					stopSignal.store(true);
+				}
+			}
+            
+            if (rng() % 2 == 0) {
+                int retval = dq->dequeue(tid);
+                if (retval == DQ_EMPTY) {
+                    bool success = dq->enqueue(tid, tid);
+                    if (success) {
+                        numOperations++;
+                    }
+                }  else {
+                    numOperations++;
+                }
+            } else {
+                bool success = dq->enqueue(tid, tid);
+                if (!success) {
+                    int retval = dq->dequeue(tid);
+                    if (retval != DQ_EMPTY) {
+                        numOperations++;
+                    }
+                } else {
+                    numOperations++;
+                }
+            }
+        }
+        #pragma omp critical
+        std::cout << "Thread #" << tid << " has completed " << numOperations << " operations..." << std::endl;
+    }
+    std::cout << "Total # of operations: " << numOperations << std::endl;
+}
+
 int main(int argc, char **argv) {
-    assert(argc == 2);
+    assert(argc == 3);
     long numNodes;
+    long seconds;
     std::istringstream(argv[1]) >> numNodes;
+    std::istringstream(argv[2]) >> seconds;
     size_t size = sizeof(DurableQueue::DurableQueue<int, DQ_EMPTY, DQ_NULL>) + numNodes * sizeof(DurableQueue::DurableQueueNode<int, DQ_NULL>);
     
-    if (numNodes > DQ_MAX_THREADS * 4) {
+    if (numNodes < DQ_MAX_THREADS * 4) {
         std::cerr << "[Warning] Hazard Pointers requires quite a bit of slack, so it is recommended that more nodes are added if this fails to complete..." << std::endl;
     }
-    
+
     // Create my heap...
     uint8_t *buffer = reinterpret_cast<uint8_t*>(std::aligned_alloc(PMAT_CACHELINE_SIZE, size));
     assert(buffer != nullptr);
 
     auto dq = DurableQueue::alloc<int, DQ_EMPTY, DQ_NULL>(buffer, size);
     sanity_check(dq, numNodes);
+    do_benchmark(dq, seconds);
 }
