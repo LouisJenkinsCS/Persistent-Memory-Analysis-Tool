a1ed152b9207d278ed804423164d2e310d6906e6
Louis Jenkins <louisjenkinscs@pop-os.localdomain>
2020-04-03 14:33:35 -0400

    Have new sequential test

---
+++ pmat/tests/microbenchmarks/durable_queue/durable_queue_ordered_test.c
@@ -0,0 +1,74 @@
+/*
+    Test to determine whether or not we can catch errors where stores are
+    written-back out-of-order due to a lack of an explicit fence.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <valgrind/pmat.h>
+#include <assert.h>
+#include "durable_queue.h"
+#include <time.h>
+
+// Size is N + 1 as we need space for the sentinel node
+#ifndef N
+#define N (1024)
+#endif
+#define SIZE (sizeof(struct DurableQueue) + (N+1) * sizeof(struct DurableQueueNode))
+
+// Note: This performs randomized enqueue/dequeue operations in lock-step due to the stop-the-world approach
+// to memory reclamation we use.
+static void do_benchmark(struct DurableQueue *dq, int seconds) {
+    time_t start;
+    time_t end;
+	time(&start);
+    while (1) {
+        for (int i = 0; i < N / 2; i++) {
+            DurableQueue_enqueue(dq,i);
+            time(&end);
+            int time_taken = end - start;
+
+            if (time_taken >= seconds) {
+                return;
+            }
+            
+        }
+        for (int i = 0; i < N / 2; i++) {
+            assert(DurableQueue_dequeue(dq, 0) == i);
+            time(&end);
+            int time_taken = end - start;
+
+            if (time_taken >= seconds) {
+                return;
+            }    
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Need a single argument (seconds), but got %d...\n", argc - 1);
+		exit(EXIT_FAILURE);
+	}
+	int seconds = atoi(argv[1]);
+	if (seconds <= 0) {
+		fprintf(stderr, "Received a time of %s seconds, but needs to be greater than 0!", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	// Disable automatic crash simulation until after queue has been initialized
+	// and sanity checking has been performed.
+	PMAT_CRASH_DISABLE();
+	void *heap;
+	assert(posix_memalign(&heap, PMAT_CACHELINE_SIZE, SIZE) == 0);
+	memset(heap, 0, SIZE);
+	PMAT_REGISTER("durable-queue.bin", heap, SIZE);
+    struct DurableQueue *dq = DurableQueue_create(heap, SIZE);
+	PMAT_CRASH_ENABLE();
+	do_benchmark(dq, seconds);
+	PMAT_CRASH_DISABLE();
+    DurableQueue_destroy(dq);
+	free(heap);
+	return 0;
+}
---
+++ pmat/tests/microbenchmarks/durable_queue/durable_queue_ordered_verifier.c
@@ -0,0 +1,45 @@
+/*
+    Run recovery and test that its all working...
+*/
+
+#include "durable_queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <valgrind/pmat.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#ifndef N
+#define N (1024 * 1024)
+#endif
+#define SIZE (sizeof(struct DurableQueue) + (N+1) * sizeof(struct DurableQueueNode))
+
+int main(int argc, char *argv[]) {
+	assert(argc >= 3);
+    assert(strcmp(argv[1], "1") == 0);
+
+    int fd = open(argv[2], O_RDONLY);
+    assert(fd != -1);
+    struct stat sb;
+    int retval = fstat(fd, &sb);
+    assert(retval != -1);
+    size_t sz = sb.st_size;
+    void *heap = mmap(NULL, sz, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+    assert(heap != (void *) -1);
+    //assert(SIZE == sz);
+    struct DurableQueue *dq = DurableQueue_recovery(heap, SIZE);
+    if (dq == NULL) return PMAT_VERIFICATION_FAILURE;
+    // Ensure ordering is okay...
+    int i = DurableQueue_dequeue(dq, 0);
+    while (i != -1) {
+        int j = DurableQueue_dequeue(dq, 0);
+        assert(j == -1 || j == i + 1);
+        i = j;
+    }
+    munmap(heap, sz);
+    return 0;
+}
